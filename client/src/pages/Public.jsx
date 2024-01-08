193448e3cfd79794c208d4200955709cd93986aa
sehyun <sehyun.lee217@gmail.com>
2024-01-07 23:07:08 -0500

    Testing backend server status

--- client/src/pages/Public.jsx
+++ client/src/pages/Public.jsx
@@ -12,7 +12,7 @@ export default function Home() {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:3500/post', {
+        fetch('http://107.20.51.100/post', {
             method: 'GET',
             credentials: "include"
         }).then(
