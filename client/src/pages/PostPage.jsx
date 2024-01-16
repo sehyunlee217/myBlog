bf9a3bf15dd4e858990f0d3fff27ff3aec742aa4
sehyun <sehyun.lee217@gmail.com>
2024-01-15 20:28:40 -0500

    Image styling

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -40,7 +40,7 @@ export default function PostPage() {
         <div className="flex items-center justify-center">
             {postData && (
                 <div>
-                    <img className="object-fill h-96" src={'https://myblogapi-410916.ue.r.appspot.com/' + postData.filePath} />
+                    <img className="object-cover h-96" src={'https://myblogapi-410916.ue.r.appspot.com/' + postData.filePath} />
                     <div className="flex justify-between items-center font-nunito">
                         <div className="text-4xl pt-3">{postData.title}</div>
                         {userInfo && (
