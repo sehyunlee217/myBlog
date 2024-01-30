9503c02b7ebf9f524ece073813f1a9ff80048b95
sehyun <sehyun.lee217@gmail.com>
2024-01-29 21:08:38 -0500

    Size changes for the post images

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -5,7 +5,7 @@ function Post({ title, image, date, summary, _id }) {
 
     return (
         <Link to={`/posts/${ _id }`} className="flex flex-col justify-between pb-10 bg-opacity-60 pt-4">
-            <img className="object-cover h-24 hover:opacity-80" src={'https://api.shyun.dev/' + image} />
+            <img className="object-cover h-36 hover:opacity-80" src={'https://api.shyun.dev/' + image} />
             <div className="flex flex-col gap-1">
                 <div className='flex justify-between align-middle'>
                     <div className="text-2xl font-abril py-2 font-bold">{title}</div>
